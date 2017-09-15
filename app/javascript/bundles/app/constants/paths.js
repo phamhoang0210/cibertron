7b2391b01c8433bb7454796e14acc06c9c28aaa7
DuongTV <duongtv@topica.edu.vn>
2017-09-15 16:03:13 +0700

    handle request unauthorized (401)

--- app/javascript/bundles/app/constants/paths.js
+++ app/javascript/bundles/app/constants/paths.js
@@ -1,5 +1,6 @@
 export const SIGN_IN_PATH = '/auth/sign_in'
 export const SIGN_UP_PATH = '/auth/sign_up'
+export const SIGN_OUT_PATH = '/auth/sign_out'
 
 export const AUTHSERVICE_BASE_URL = process.env.NODE_ENV == 'production' ? 'https://authservice.edumall.io' : 'http://localhost:9000'
 export const USERSERVICE_BASE_URL = process.env.NODE_ENV == 'production' ? 'https://userservice.edumall.io' : 'http://localhost:9001'
