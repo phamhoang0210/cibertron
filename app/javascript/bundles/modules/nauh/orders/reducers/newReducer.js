e1ecc407cbb155af1c64eb9daed19b25afe10a82
DuongTV <duongtv@topica.edu.vn>
2017-10-02 14:53:14 +0700

    NAUH: support payment method when create order

--- app/javascript/bundles/modules/nauh/orders/reducers/newReducer.js
+++ app/javascript/bundles/modules/nauh/orders/reducers/newReducer.js
@@ -7,6 +7,23 @@ export const initialState = Immutable.fromJS({
   isCreatingOrder: false,
   isFetchingLead: false,
   lead: null,
+  paymentMethods: [
+    { value: 'COD', title: 'COD' },
+    { value: 'TRANSFER', title: 'TRANSFER' },
+    { value: 'OFFICE', title: 'OFFICE' },
+    { value: 'ONE_PAY', title: 'ONE_PAY' },
+  ],
+  transferBanks: [
+    { value: "Argribank", title: "Agribank - Ngân hàng Nông nghiệp và Phát triển Nông thôn Việt Nam" },
+    { value: "Vpbank", title: "Vpbank - Ngân hàng Việt Nam Thịnh Vượng" },
+    { value: "ACB", title: "ACB - Ngân hàng Á Châu" },
+    { value: "BIDV", title: "BIDV - Ngân hàng TMCP Đầu Tư & Phát Triển Việt Nam" },
+    { value: "Vietcombank", title: "Vietcombank - Ngân hàng thương mại cổ phần Ngoại thương" },
+  ],
+  officeAddress: [
+    { value: 'add_1', title: '75 Phương Mai, Đống Đa, Hà Nội'},
+    { value: 'add_2', title: '58/10 Thành Thái, Phường 12, Quận 10, HCM'},
+  ]
 })
 
 export default function newReducer($$state = initialState, action = null) {
