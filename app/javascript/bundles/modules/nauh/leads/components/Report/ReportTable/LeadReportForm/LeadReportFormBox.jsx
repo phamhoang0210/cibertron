db8386310dbc153cd0d4ac0ad2a6a710beea1c1a
minhlekhac <minhlekhac@gmail.com>
2018-04-05 16:24:28 +0700

    Report data

--- app/javascript/bundles/modules/nauh/leads/components/Report/ReportTable/LeadReportForm/LeadReportFormBox.jsx
+++ app/javascript/bundles/modules/nauh/leads/components/Report/ReportTable/LeadReportForm/LeadReportFormBox.jsx
@@ -33,31 +33,7 @@ class LeadReportFormBox extends React.Component {
     const { getFieldDecorator } = this.props.form
 
     return (
-      <Form layout="inline" onSubmit={this.handleSubmit}>
-        <FormItem label={intl.formatMessage({id: 'report.leads_form.form_item.from.label'})}>
-            <Input
-              style={{ width: '300px' }}
-              type="number"
-              placeholder={intl.formatMessage({id: 'report.leads_form.form_item.from.placeholder.input'})}
-            />
-        </FormItem>
-        <FormItem label={intl.formatMessage({id: 'report.leads_form.form_item.to.label'})}>
-            <Input
-                style={{ width: '300px' }}
-                type="number"
-                placeholder={intl.formatMessage({id: 'report.leads_form.form_item.to.placeholder.input'})}
-            />
-        </FormItem>
-        <FormItem>
-          <Button
-            type="primary"
-            htmlType="submit"
-            loading={isReportingLeads}
-          >
-            {intl.formatMessage({id: 'form.form_item.button.filter.text'})}
-          </Button>
-        </FormItem>
-      </Form>
+      null
     )
   }
 }
