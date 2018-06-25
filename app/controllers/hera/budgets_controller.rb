f5d4025d11f369f7da3c848f44c364a76f989bc5
Duc Vo Van <ducvv@topica.edu.vn>
2018-06-25 18:13:45 +0700

    Create budget for marketer

---
+++ app/controllers/hera/budgets_controller.rb
@@ -0,0 +1,16 @@
+module Hera
+  class BudgetsController < HeraController
+    # GET /hera/landing_pages
+    def index
+    end
+
+    # GET /hera/landing_pages/:id/edit
+    def edit
+    end
+
+    # GET /hera/landing_pages/new
+    def new
+    end
+
+  end
+end
