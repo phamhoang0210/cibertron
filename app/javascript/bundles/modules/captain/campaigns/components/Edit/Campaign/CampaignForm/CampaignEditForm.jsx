42528db23c32d4aa5c39b0d1f0b51ba4b89e76c8
khangpt <khangpt@topica.edu.vn>
2018-08-13 21:03:48 +0700

    [Captain] - handle some actions for edit campaign

--- app/javascript/bundles/modules/captain/campaigns/components/Edit/Campaign/CampaignForm/CampaignEditForm.jsx
+++ app/javascript/bundles/modules/captain/campaigns/components/Edit/Campaign/CampaignForm/CampaignEditForm.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import {Table, Form, Row, Col, Select, DatePicker, Button, Input, Icon, Checkbox, Radio} from 'antd'
-import { formItemLayout, DEFAULT_TITLE_LAYOUT, DEFAULT_SUBTITLE_LAYOUT, FORM_SELECT_COURSES, LIST_SELECTED_COURSES } from 'app/constants/form'
+import { browserHistory } from 'react-router'
+import {Form, Row, Col, Select, DatePicker, Button, Input, Radio} from 'antd'
+import { formItemLayout, DEFAULT_TITLE_LAYOUT } from 'app/constants/form'
 import { formDate } from 'app/constants/form'
 import { injectIntl } from 'react-intl'
+import { CAMPAIGNS_URL } from '../../../../constants/paths'
 
 const FormItem = Form.Item;
 const RadioGroup = Radio.Group;
-const Search = Input.Search
-const { RangePicker } = DatePicker;
 
 class CampaignEditForm extends React.Component {
   constructor(props) {
     super(props)
+
+    _.bindAll(this, [
+      'handleBack',
+      'handleUpdate',
+    ])
   }
 
   handleSubmit = (e) => {
@@ -27,70 +32,29 @@ class CampaignEditForm extends React.Component {
     console.log('checked = ${e.target.checked}');
   }
 
+  handleBack(e) {
+    browserHistory.push(`${CAMPAIGNS_URL}`)
+  }
+
+  handleUpdate(e) {
+    e.preventDefault()
+    console.log('props', this.props)
+    const {actions, editState} = this.props
+    console.log('Hello', editState)
+    var campaignId = editState.getIn(['campaign', '_id', '$oid'])
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        actions.updateCampaign(campaignId, {record: values})
+      }
+    })
+  }
+
   render(){
     const {intl,editState,sharedState} = this.props
     const left_data = editState['left_records']
     const right_data = editState['right_records']
     const { getFieldDecorator } = this.props.form
-    const left_columns = [
-      {
-        title: 'Select',
-        key: 'select',
-        render: (text, record) => (
-          <Checkbox onChange={this.onChange}></Checkbox>
-        )
-      },
-      {
-        title: 'Mã khóa',
-        dataIndex: 'course_code',
-        key: 'course_code',
-      },
-      {
-        title: 'Tên khóa',
-        dataIndex: 'course_name',
-        key: 'course_name',
-      },
-      {
-        title: 'Giá gốc',
-        dataIndex: 'price',
-        key: 'price',
-      }
-    ];
-    const right_columns = [
-      {
-        title: 'Khóa',
-        dataIndex: 'course_code',
-        key: 'course_code',
-      },
-      {
-        title: 'Giá gốc',
-        dataIndex: 'price',
-        key: 'price',
-      },
-      {
-        title: 'Giá giảm',
-        dataIndex: 'promotion_price',
-        key: 'promotion_price',
-        render: (text, record) => (
-          <Input placeholder="" />
-        )
-      },
-      {
-        title: '% giảm',
-        dataIndex: '10',
-        key: '10',
-      },
-      {
-        title: 'Action',
-        key: 'action',
-        render: (text, record) => (
-          <div>
-            <Button style={{marginRight:10}} type="primary">Sửa</Button>
-            <Button type="danger">Xóa</Button>
-          </div>
-        )
-      }
-    ];
+    
     return(
       <Form onSubmit={this.handleSubmit}>
         <Row>
@@ -112,7 +76,7 @@ class CampaignEditForm extends React.Component {
               {getFieldDecorator('startTime', {
                 rules: [{ required: true,message: intl.formatMessage({id: 'attrs.time_start.required'},) }],
               })(
-                <DatePicker style={{width: '100%'}} placeholder={intl.formatMessage({id: 'attrs.time_start.placeholder.select.none'})} showTime placeholder="Select Time" format="YYYY-MM-DD HH:mm:ss"/>
+                <DatePicker style={{width: '100%'}} placeholder={intl.formatMessage({id: 'attrs.time_start.placeholder.select.none'})} showTime placeholder="Select Time" format="YYYY-MM-DD HH:mm"/>
               )}
             </FormItem>
           </Col>
@@ -121,7 +85,7 @@ class CampaignEditForm extends React.Component {
               {getFieldDecorator('endTime', {
                 rules: [{ required: true,message: intl.formatMessage({id: 'attrs.time_end.required'},) }],
               })(
-                <DatePicker style={{width: '100%'}} placeholder={intl.formatMessage({id: 'attrs.time_end.placeholder.select.none'})} showTime placeholder="Select Time" format="YYYY-MM-DD HH:mm:ss"/>
+                <DatePicker style={{width: '100%'}} placeholder={intl.formatMessage({id: 'attrs.time_end.placeholder.select.none'})} showTime placeholder="Select Time" format="YYYY-MM-DD HH:mm"/>
               )}
             </FormItem>
           </Col>
@@ -168,69 +132,8 @@ class CampaignEditForm extends React.Component {
 
         <Row>
           <Col span={24} style={{ textAlign: 'center' }}>
-            <Button style={{marginRight:10}}>Hủy bỏ</Button>
-            <Button type="primary" htmlType="submit" style={{marginRight:10}}>Update</Button>
-          </Col>
-        </Row>
-
-        <br />
-        <hr />
-
-        <Row>
-          <FormItem {...DEFAULT_TITLE_LAYOUT} style={{marginLeft:15, fontWeight:'bold'}} label={intl.formatMessage({id: 'edit.manage_deal.label'})} ></FormItem>
-        </Row>
-
-        <Row>
-          <Col span={12}>
-            <FormItem {...DEFAULT_SUBTITLE_LAYOUT} style={{marginLeft:15}} label={intl.formatMessage({id: 'edit.select_courses.label'})} ></FormItem>
-            <Col span={24}>
-              <FormItem label={intl.formatMessage({id: 'edit.search_courses_by.label'})}
-                {...FORM_SELECT_COURSES}>
-                  <Select placeholder={intl.formatMessage({id: 'edit.search_courses_by.placeholder.select.none'})} >
-                    <Option value="category">Category</Option>
-                    <Option value="teacher">Thầy</Option>
-                    <Option value="price">Giá bán</Option>
-                    <Option value="course_code">Mã khóa</Option>
-                  </Select>
-              </FormItem>
-            </Col>
-            <Col span={24}>
-              <FormItem label={intl.formatMessage({id: 'edit.condition.label'})}
-                {...FORM_SELECT_COURSES}>
-                  <Search
-                    enterButton
-                  />
-              </FormItem>
-            </Col>
-            <Col span={23}>
-              <Table
-                className="components-table-demo-nested"
-                columns={left_columns}
-                dataSource={left_data}
-                bordered
-              />
-            </Col>
-          </Col>
-
-          <Col span={12}>
-            <FormItem {...DEFAULT_SUBTITLE_LAYOUT} style={{marginLeft:15}} label={intl.formatMessage({id: 'edit.list_selected_courses.label'})} ></FormItem>
-            <Col span={24}>
-              <Table
-                className="components-table-demo-nested"
-                columns={right_columns}
-                dataSource={right_data}
-                bordered
-              />
-            </Col>
-          </Col>
-        </Row>
-
-        <br />
-
-        <Row>
-          <Col span={24} style={{ textAlign: 'center' }}>
-            <Button style={{marginRight:10}}>Hủy bỏ</Button>
-            <Button type="primary" htmlType="submit" style={{marginRight:10}}>Lưu lại</Button>
+            <Button style={{marginRight:10}} onClick={this.handleBack} >Hủy bỏ</Button>
+            <Button onClick={this.handleUpdate} type="primary" htmlType="submit" style={{marginRight:10}}>Update</Button>
           </Col>
         </Row>
       </Form>
---
+++ app/javascript/bundles/modules/captain/campaigns/components/Edit/Campaign/CampaignForm/DetailCampaignEditForm.jsx
@@ -0,0 +1,162 @@
+import React from 'react'
+import { browserHistory } from 'react-router'
+import {Table, Form, Row, Col, Select, Button, Input, Checkbox, Radio} from 'antd'
+import { DEFAULT_TITLE_LAYOUT, DEFAULT_SUBTITLE_LAYOUT, FORM_SELECT_COURSES, LIST_SELECTED_COURSES } from 'app/constants/form'
+import { injectIntl } from 'react-intl'
+import { CAMPAIGNS_URL } from '../../../../constants/paths'
+
+const FormItem = Form.Item;
+const Search = Input.Search
+
+class DetailCampaignEditForm extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  onChange = (e) => {
+    console.log('checked = ${e.target.checked}');
+  }
+
+  handleBack(e) {
+    browserHistory.push(`${CAMPAIGNS_URL}`)
+  }
+
+  render(){
+    const {intl,editState,sharedState} = this.props
+    const left_data = editState['left_records']
+    const right_data = editState['right_records']
+    const { getFieldDecorator } = this.props.form
+    const left_columns = [
+      {
+        title: 'Select',
+        key: 'select',
+        render: (text, record) => (
+          <Checkbox onChange={this.onChange}></Checkbox>
+        )
+      },
+      {
+        title: 'Mã khóa',
+        dataIndex: 'course_code',
+        key: 'course_code',
+      },
+      {
+        title: 'Tên khóa',
+        dataIndex: 'course_name',
+        key: 'course_name',
+      },
+      {
+        title: 'Giá gốc',
+        dataIndex: 'price',
+        key: 'price',
+      }
+    ];
+    const right_columns = [
+      {
+        title: 'Khóa',
+        dataIndex: 'course_code',
+        key: 'course_code',
+      },
+      {
+        title: 'Giá gốc',
+        dataIndex: 'price',
+        key: 'price',
+      },
+      {
+        title: 'Giá giảm',
+        dataIndex: 'promotion_price',
+        key: 'promotion_price',
+        render: (text, record) => (
+          <Input placeholder="" />
+        )
+      },
+      {
+        title: '% giảm',
+        dataIndex: '10',
+        key: '10',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <div>
+            <Button style={{marginRight:10}} type="primary">Sửa</Button>
+            <Button type="danger">Xóa</Button>
+          </div>
+        )
+      }
+    ];
+    return(
+      <Form onSubmit={this.handleSubmit}>
+
+        <Row>
+          <FormItem {...DEFAULT_TITLE_LAYOUT} style={{marginLeft:15, fontWeight:'bold'}} label={intl.formatMessage({id: 'edit.manage_deal.label'})} ></FormItem>
+        </Row>
+
+        <Row>
+          <Col span={12}>
+            <FormItem {...DEFAULT_SUBTITLE_LAYOUT} style={{marginLeft:15}} label={intl.formatMessage({id: 'edit.select_courses.label'})} ></FormItem>
+            <Col span={24}>
+              <FormItem label={intl.formatMessage({id: 'edit.search_courses_by.label'})}
+                {...FORM_SELECT_COURSES}>
+                  <Select placeholder={intl.formatMessage({id: 'edit.search_courses_by.placeholder.select.none'})} >
+                    <Option value="category">Category</Option>
+                    <Option value="teacher">Thầy</Option>
+                    <Option value="price">Giá bán</Option>
+                    <Option value="course_code">Mã khóa</Option>
+                  </Select>
+              </FormItem>
+            </Col>
+            <Col span={24}>
+              <FormItem label={intl.formatMessage({id: 'edit.condition.label'})}
+                {...FORM_SELECT_COURSES}>
+                  <Search
+                    enterButton
+                  />
+              </FormItem>
+            </Col>
+            <Col span={23}>
+              <Table
+                className="components-table-demo-nested"
+                columns={left_columns}
+                dataSource={left_data}
+                bordered
+              />
+            </Col>
+          </Col>
+
+          <Col span={12}>
+            <FormItem {...DEFAULT_SUBTITLE_LAYOUT} style={{marginLeft:15}} label={intl.formatMessage({id: 'edit.list_selected_courses.label'})} ></FormItem>
+            <Col span={24}>
+              <Table
+                className="components-table-demo-nested"
+                columns={right_columns}
+                dataSource={right_data}
+                bordered
+              />
+            </Col>
+          </Col>
+        </Row>
+
+        <br />
+
+        <Row>
+          <Col span={24} style={{ textAlign: 'center' }}>
+            <Button onClick={this.handleBack} style={{marginRight:10}}>Hủy bỏ</Button>
+            <Button type="primary" htmlType="submit" style={{marginRight:10}}>Lưu lại</Button>
+          </Col>
+        </Row>
+      </Form>
+    )
+  }
+}
+
+export default Form.create()(injectIntl(DetailCampaignEditForm))
