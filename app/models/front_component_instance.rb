8e23929ee19901ba6e41a271745753e221944763
dieupv <dieupv@topica.edu.vn>
2018-08-15 18:26:09 +0700

    upgrade front component management

--- app/models/front_component_instance.rb
+++ app/models/front_component_instance.rb
@@ -1,8 +1,20 @@
 class FrontComponentInstance < ApplicationRecord
   # Define relations
   belongs_to :front_component, optional: true
-  has_many :front_component_dist
+  has_many :front_component_dists, dependent: :destroy
 
   # Define constraints
   validates :code, presence: true
+
+  class << self
+    def compcond_columns
+      [:id, :name, :title]
+    end
+
+    def include_entities
+      {
+        FrontComponentDist => [:front_component_dists],
+      }
+    end
+  end
 end
