c1223e5bd053acc7c1bb08d599f697846bb0e97b
khangpt <khangpt@topica.edu.vn>
2018-08-21 10:29:26 +0700

    [Captain] - Update edit action

--- app/javascript/bundles/modules/captain/campaigns/constants/paths.js
+++ app/javascript/bundles/modules/captain/campaigns/constants/paths.js
@@ -5,3 +5,8 @@ export const CAMPAIGNS_API_PATH = '/api/v1/campaign'
 export const USERS_API_PATH = '/api/v1/campaign/?fields=[creator]'
 export const ALL_CAMPAIGNS_API_PATH = '/api/v1/campaign/?fields=[name]'
 
+export const COURSES_API_PATH = '/api/v1/courses'
+export const CATALOGS_API_PATH = '/api/v1/catalogs'
+export const INSTRUCTORS_API_PATH = '/api/v1/instructors'
+
+
