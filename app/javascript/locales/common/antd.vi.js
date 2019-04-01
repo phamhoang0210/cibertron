46f086a76755afefa846b86239b48f35056bf9e0
khanhtt <khanhtt2@topica.edu.vn>
2019-04-01 16:03:17 +0700

    fix bug ui

--- app/javascript/locales/common/antd.vi.js
+++ app/javascript/locales/common/antd.vi.js
@@ -7,31 +7,43 @@ export default flattenMessages({
       button: {
         create: {
           // form.form_item.button.create.text
-          text: 'Tạo',
+          text: 'Create',
+        },
+        cancel: {
+          // form.form_item.button.cancel.text
+          text: 'Cancel',
+        },
+        close: {
+          // form.form_item.button.close.text
+          text: 'Close',
         },
         done: {
           // form.form_item.button.done.text
-          text: 'Xong',
+          text: 'Done',
+        },
+        order: {
+          // form.form_item.button.order.text
+          text: 'Order',
         },
         update: {
           // form.form_item.button.update.text
-          text: 'Cập nhật',
+          text: 'Update',
         },
         restore: {
           // form.form_item.button.restore.text
-          text: 'Khôi phục',
+          text: 'Restore',
         },
         back: {
           // form.form_item.button.back.text
-          text: 'Trở lại',
+          text: 'Back',
         },
         delete: {
           // form.form_item.button.delete.text
-          text: 'Xóa',
+          text: 'Delete', 
         },
         edit: {
           // form.form_item.button.edit.text
-          text: 'Sửa',
+          text: 'Edit',
         },
         clear: {
           // form.form_item.button.clear.text
@@ -39,63 +51,31 @@ export default flattenMessages({
         },
         filter: {
           // form.form_item.button.filter.text
-          text: 'Lọc',
+          text: 'Filter',
         },
         add: {
           // form.form_item.button.add.text
-          text: 'Thêm',
+          text: 'Add',
         },
         import: {
           // form.form_item.button.import.text
-          text: 'Nhập',
+          text: 'Import',
         },
         assign: {
           // form.form_item.button.assign.text
-          text: 'Phân chia',
-        },
-        report: {
-          // form.form_item.button.assign.text
-          text: 'Báo cáo năng suất',
-        },
-        recovery: {
-          // form.form_item.button.assign.text
-          text: 'Thu hồi',
+          text: 'Assign',
         },
         get_code: {
           // form.form_item.button.get_code.text
-          text: 'Lấy mã',
-        },
-        call: {
-          // form.form_item.button.call.text
-          text: 'Gọi',
-        },
-        send_email: {
-          // form.form_item.button.send_email.text
-          text: 'Gửi email',
+          text: 'Get code',
         },
         email_wrong_phone_number: {
           // form.form_item.button.email_wrong_phone_number.text
-          text: 'Gửi email sai số',
+          text: 'Wrong phone number',
         },
         email_no_answer: {
           // form.form_item.button.email_no_answer.text
-          text: 'Gửi email không nghe máy',
-        },
-        email_schedule: {
-          // form.form_item.button.email_schedule.text
-          text: 'Gửi email hẹn đến office',
-        },
-        email_online_payment_guide: {
-          // form.form_item.button.email_online_payment_guide.text
-          text: 'Gửi email hướng dẫn thanh toán trực tuyến',
-        },
-        email_counseling_course: {
-          // form.form_item.button.email_counseling_course.text
-          text: 'Gửi email tư vấn khóa học',
-        },
-        eros: {
-          // form.form_item.button.eros.text
-          text: 'Eros',
+          text: 'No answer',
         },
       },
     },
