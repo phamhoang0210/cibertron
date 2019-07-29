dce6b7064fe38d855ca2867864a3fe2eb5693b28
HoangPV <hoangpv@topica.edu.vn>
2019-07-29 14:20:12 +0700

    Task name: Fixbug discount
    Description: fixbug discount
    
    Link Jira:  https://jira.topica.vn/browse/IEMS-1622

--- app/javascript/bundles/modules/sol/discounts/components/New/NewScreen.jsx
+++ app/javascript/bundles/modules/sol/discounts/components/New/NewScreen.jsx
@@ -8,8 +8,8 @@ class NewScreen extends React.Component {
 
   componentDidMount() {
     const {actions} = this.props
-    actions.fetchCourses({per_page: 'product_json'})
-    actions.fetchCombos({per_page: 'product_json'})
+    actions.fetchCourses({fields: 'product_json'})
+    actions.fetchCombos({fields: 'product_json'})
   }
 
   render() {
