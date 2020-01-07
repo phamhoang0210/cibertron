0f4ece6e7bd6df10953d7b341e4f5b2257b6ccca
Meowth <hoangpv>
2020-01-07 16:54:36 +0700

    add db migrate account

--- app/models/service_info.rb
+++
@@ -1,7 +0,0 @@
-class ServiceInfo < ApplicationRecord
-  module ForeignKeys
-    COMPANY_ID = :company_id
-    DEPARTMENT_ID = :department_id
-    USER_ID = :user_id
-  end
-end
--- db/migrate/20180815092657_upgrade_table_front_component_instances.rb
+++
@@ -1,5 +0,0 @@
-class UpgradeTableFrontComponentInstances < ActiveRecord::Migration[5.1]
-  def change
-    add_column :front_component_instances, :url_publish, :string, limit: 2047
-  end
-end
